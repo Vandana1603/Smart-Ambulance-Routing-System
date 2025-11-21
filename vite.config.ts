53f29d5a1b60894341e00462622024d22e082ca2
VANDANA K <vandanakbng@gmail.com>
2025-11-21 10:48:17 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // import { componentTagger } from "D:\FINAL\smart.jpg";
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/my-repo-name/',
   server: {
     host: "::",
     port: 8080,
