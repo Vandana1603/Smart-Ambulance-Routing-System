44ba80ba6275b5a8c4567e2392cfecce5b4f5ea5
VANDANA K <vandanakbng@gmail.com>
2025-11-22 11:30:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'Smart-Ambulance-Routing-System',
+  base: '/smartambulanceroutingsystem/',
   server: {
     host: "::",
     port: 8080,
